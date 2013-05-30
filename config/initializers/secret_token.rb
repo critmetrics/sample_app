2d436bfe4c4fb4f202998193e9db1b44fcd17d00
John Larkin <john@critmetrics.com>
2013-05-30 23:13:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '087c7a3bc0fa711bf2e830c350b9fb788751d1430b86ca94607bb0c1ceb431ccbd4fce9f619e3dcaa18ac23f22dcefb7d98d4ba5080dd78da27f2acc73c32adf'
